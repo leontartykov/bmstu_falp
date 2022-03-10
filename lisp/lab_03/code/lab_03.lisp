9451d630e4372316f33d268db3a2ac1c48b0d239
Lev Tartykov <lev_tartykov@mail.ru>
2022-03-10 22:10:09 +0300

    Переписаны программы для проверки серединного элемента

--- lisp/lab_03/code/lab_03.lisp
+++ lisp/lab_03/code/lab_03.lisp
@@ -19,17 +19,29 @@
 
 ;4. функция, которая принимает три числа и возвращает T только
 ;тогда, когда первое число расположено между вторым и третьим
-(defun is_middle_cond (number_1 number_2 number_3)
-    (cond ((and (> number_1 number_2) (< number_1 number_3)) T) (T Nil)))
+(defun is_middle_cond_new (number_1 number_2 number_3)
+    (cond ((> number_1 number_2) (cond ((< number_1 number_3) T)))
+          ((< number_1 number_2) (cond ((> number_1 number_3) T)))
+    )
+)
+(print (is_middle_cond_new 4 2 6))
+(print (is_middle_cond_new 4 6 2))
 
 (defun is_middle_if (number_1 number_2 number_3)
     (if (and (> number_1 number_2) (< number_1 number_3)) T nil))
 
 (defun is_middle_andor (number_1 number_2 number_3)
-    (and (> number_1 number_2) (< number_1 number_3)))
-(print (is_middle_cond 4 2 5))
+    (or (and (> number_1 number_2) (< number_1 number_3))
+        (and (> number_1 number_2) (< number_1 number_3))
+    )
+)
+
+
 (print (is_middle_if 4 2 5))
 (print (is_middle_andor 2 4 5))
+
+
+
 
 ;5. предикат, который принимает два числа-аргумента и возвращает T,
 ;если первое число не меньше второго.
@@ -39,11 +51,13 @@
 
 ;6. переписанная функция how_alike
 ;how_alike cond
-(defun how_alike_cond(x y)
+(defun how_alike(x y)
     (cond ((or (= x y) (equal x y)) 'the_same)
     ((and (oddp x) (oddp y)) 'both_odd)
     ((and (evenp x) (evenp y)) 'both_even)
     (t 'difference) ))
+
+
 
 ;how_alike if, and/or
 (defun how_alike_if_andor(x y)
@@ -77,3 +91,19 @@
 (print (how_alike_if 4 2))
 (print (how_alike_if 1 1))
 (print (how_alike_if 5 7))
+
+(defun how_alike_cond (x y)
+    (cond ((= x y) (cond ((equal x y) 'the_same)))
+          ((oddp x) (cond ((oddp y) 'both_odd) (T 'difference)))
+          ((evenp x) (cond ((evenp y) 'both_even) (T 'difference)))
+          ('difference)
+    )
+)
+(print (how_alike_cond 1 1))
+(print (how_alike_cond 4 2))
+(print (how_alike_cond 5 7))
+(print (how_alike_cond -3 2))
+(print (how_alike_cond -2 3))
+
+
+
